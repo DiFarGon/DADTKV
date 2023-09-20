c9f4efbda3156504f720af2115d6a314df402fc3
DiFarGon <diogo.faria.goncalves@tecnico.ulisboa.pt>
2023-09-20 17:21:40 +0100

    Add project files.

---
+++ dadtkv/Client/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace dadtkv.Client
+{
+    internal class Client
+    {
+        private readonly string id;
+
+        public Client(string id)
+        {
+            this.id = id;
+        }
+    }
+
+    internal class Program
+    {
+        
+    }
+}
---
+++ dadtkv/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Dadtkv
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Error.WriteLine("Not enough arguments!");
+                return;
+            }
+
+            string[] script = File.ReadAllLines(args[0]);
+            foreach (string line in script)
+            {
+                switch (line[0])
+                {
+                    case '#':
+                        continue;
+                    case 'P':
+                        handleP(line);
+                        break;
+                    case 'S':
+                        handleS(line);
+                        break;
+                    case 'D':
+                        handleD(line);
+                        break;
+                    case 'T':
+                        handleT(line);
+                        break;
+                    case 'F':
+                        handleF(line);
+                        break;
+                }
+            }
+            return;
+        }
+
+        private static void handleP(string line)
+        {
+            string[] tokens = line.Split(' ');
+            string id = tokens[1];
+            string type = tokens[2];
+            string url = tokens[3];
+
+            switch (type)
+            {
+                case "T":
+                    newTransactionManager(id, url);
+                    break;
+                case "L":
+                    newLeaseManager(id, url);
+                    break;
+                case "C":
+                    newClient(id, url);
+                    break;
+            }
+        }
+
+        private static void handleS(string line)
+        {
+            string slots = line.Split(' ')[1];
+
+            Console.WriteLine("Test lasts {0} slots", slots);
+        }
+
+        private static void handleD(string line)
+        {
+            string time = line.Split(' ')[1];
+
+            Console.WriteLine("Each slot lasts {0} miliseconds", time);
+        }
+
+        private static void handleT(string line)
+        {
+            string time = line.Split(' ')[1];
+
+            Console.WriteLine("Test starts at {0}", time);
+        }
+
+        private static void handleF(string line)
+        {
+            Console.WriteLine("I don't really want to do this right now");
+        }
+
+        private static void newTransactionManager(string id, string url)
+        {
+            Console.WriteLine("Creating new transaction manager with id {0} and url {1}", id, url);
+        }
+
+        private static void newLeaseManager(string id, string url)
+        {
+            Console.WriteLine("Creating new lease manager with id {0} and url {1}", id, url);
+        }
+
+        private static void newClient(string id, string url)
+        {
+            Console.WriteLine("Creating new client with id {0} and url {1}", id, url);
+        }
+    }
+}
