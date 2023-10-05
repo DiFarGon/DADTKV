34aadfe53b9630b65873aec5aae4d7500a0c4ed1
DiFarGon <diogo.faria.goncalves@tecnico.ulisboa.pt>
2023-10-05 20:52:50 +0100

    added missing arguments to transaction manager

--- TransactionManager/Program.cs
+++ TransactionManager/Program.cs
@@ -8,14 +8,21 @@ namespace TransactionManager
     {
         public static void Main(string[] args)
         {
-            if (args.Length != 2)
+            // <clusterId> <id> <url> <lms> <tms> <debug?>
+
+            if (args.Length < 5 || args.Length > 6)
             {
-                Console.Error.WriteLine("wrong arguments!");
+                Console.Error.WriteLine("[TransactionManager] Wrong number of arguments!");
                 return;
             }
-            TransactionManager transactionManager = new TransactionManager(args[0], args[1]);
 
-            var uri = new Uri(args[1]);
+            bool debug = false;
+            if (args.Length == 6 && args[5] == "debug")
+                debug = true;
+
+            TransactionManager transactionManager = new TransactionManager(int.Parse(args[0]), args[1], args[2], debug);
+
+            var uri = new Uri(args[2]);
             string host = uri.Host;
             int port = uri.Port;
 
